fcb007dfb6667b53d17fc9281ba681a7c50c54f6
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-11 23:42:36 -0500

    created more tests

--- cypress/integration/TGTest.spec.js
+++ cypress/integration/TGTest.spec.js
@@ -1,13 +1,30 @@
 /// <reference types="cypress"/>
 
-it('should be able to add a new todo to the list', () => {
+it('should be able to see TG Logo', () => {
     cy.visit('https://techglobal-training.com/') // Adding '?delay-new-todo=5000' to the end of the URL will delay the 'new-todo' element from appearing for 5 seconds
 
-    // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
+     // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
+    cy.get('.Header_headerContainer__kKEW0 > :nth-child(1) > #logo > img').should('not.be.hidden')
+})
+
+it('should be able to see TG Footer', () => {
+    cy.visit('https://techglobal-training.com/')
+
+    cy.get('.has-text-centered > :nth-child(1) > p').should('be.visible')
+    cy.get('.has-text-centered > :nth-child(1) > p').should('have.text', '2860 S River Rd Suite 480, Des Plaines, IL 60018')
+
+    cy.get('#Footer_facebook__UiT2a > path').should('be.visible')
+})
+
+/*
+it('should be able to fill out mock interview form', () => {
+    cy.visit('https://techglobal-training.com/') 
+
     cy.get('.navbar-brand > .active').click()
     cy.get('#mock-option').click()
 
     cy.get(':nth-child(2) > input').type('ramiturkmani')
     cy.get(':nth-child(4) > input').type('techglobal123')
     cy.get('button').click()
-})
+}) 
+*/
