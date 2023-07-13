0daf2aa5d1e5f19a3eb2a77065d07dc3ec4a8cc3
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-13 00:45:42 -0500

    integrated mocha test grouping

--- cypress/integration/TGLogoAndFooter.spec.js
+++ cypress/integration/TGLogoAndFooter.spec.js
@@ -1,24 +1,27 @@
 /// <reference types="cypress"/>
 
-it('should be able to see TG Logo', () => {
-    cy.visit('https://techglobal-training.com/') // Adding '?delay-new-todo=5000' to the end of the URL will delay the 'new-todo' element from appearing for 5 seconds
+describe('home page verification', () => {
 
-     // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
-    cy.get('.Header_headerContainer__kKEW0 > :nth-child(1) > #logo > img').should('not.be.hidden')
-})
+    beforeEach(() => {
+        cy.visit('https://techglobal-training.com/') // Adding '?delay-new-todo=5000' to the end of the URL will delay the 'new-todo' element from appearing for 5 seconds
+    })
 
-it('should be able to see TG Footer', () => {
-    cy.visit('https://techglobal-training.com/')
+    it('should be able to see TG Logo', () => {
+        cy.get('.Header_headerContainer__kKEW0 > :nth-child(1) > #logo > img').should('be.visible')
+        // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
+    })
 
-    cy.get('.has-text-centered > :nth-child(1) > p').should('be.visible')
-    cy.get('.has-text-centered > :nth-child(1) > p').should('have.text', '2860 S River Rd Suite 480, Des Plaines, IL 60018')
+    it('should be able to see TG Footer', () => {
+        cy.get('.has-text-centered > :nth-child(1) > p').should('be.visible')
+        cy.get('.has-text-centered > :nth-child(1) > p').should('have.text', '2860 S River Rd Suite 480, Des Plaines, IL 60018')
 
-    cy.get('#Footer_facebook__UiT2a > path').should('be.visible')
-    cy.get('[href="https://www.facebook.com/techglobaleducation"]').should('not.be.empty')
+        cy.get('#Footer_facebook__UiT2a > path').should('be.visible')
+        cy.get('[href="https://www.facebook.com/techglobaleducation"]').should('not.be.empty')
 
-    cy.get('#Footer_instagram__0NXVy').should('be.visible')
-    cy.get('[href="https://www.instagram.com/techglobal.school/?hl=en"]').should('not.be.empty')
+        cy.get('#Footer_instagram__0NXVy').should('be.visible')
+        cy.get('[href="https://www.instagram.com/techglobal.school/?hl=en"]').should('not.be.empty')
 
-    cy.get('.has-text-centered > :nth-child(3) > p').should('be.visible')
-    cy.get('.has-text-centered > :nth-child(3) > p').should('have.text', '© 2023 TechGlobal - All Rights Reserved')
+        cy.get('.has-text-centered > :nth-child(3) > p').should('be.visible')
+        cy.get('.has-text-centered > :nth-child(3) > p').should('have.text', '© 2023 TechGlobal - All Rights Reserved')
+    })
 })
--- cypress/integration/TGMockInterview.spec.js
+++ cypress/integration/TGMockInterview.spec.js
@@ -1,16 +1,12 @@
 /// <reference types="cypress"/>
 
-describe('mock interview functionality', () => {
-    it('should be able to navigate to the mock interview page', () => {
+it('should be able to navigate to the mock interview page', () => {
         cy.visit('https://techglobal-training.com/')
-    
+
         cy.get('.navbar-brand > .active').click()
         cy.get('#mock-option').click()
-    })
-    
-    it('should be able to fill out mock interview form', () => {
+        
         cy.get(':nth-child(2) > input').type('ramiturkmani')
         cy.get(':nth-child(4) > input').type('techglobal123')
         cy.get('button').click()
-    }) 
-})
+    })
