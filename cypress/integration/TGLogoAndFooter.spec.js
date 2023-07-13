48878f94351ed528dc5a8b94cd570d23dd0996b3
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-13 09:47:44 -0500

    implemented headless testing and Page Object Model

--- cypress/integration/TGLogoAndFooter.spec.js
+++ cypress/integration/TGLogoAndFooter.spec.js
@@ -1,27 +1,25 @@
 /// <reference types="cypress"/>
 
+import { TGHomePage } from '../page_objects/TGHomePage.js';
+
 describe('home page verification', () => {
+    const homePage = new TGHomePage()
 
     beforeEach(() => {
-        cy.visit('https://techglobal-training.com/') // Adding '?delay-new-todo=5000' to the end of the URL will delay the 'new-todo' element from appearing for 5 seconds
+        homePage.navigate()
     })
 
     it('should be able to see TG Logo', () => {
-        cy.get('.Header_headerContainer__kKEW0 > :nth-child(1) > #logo > img').should('be.visible')
-        // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
+        homePage.logoVisibility()
     })
 
     it('should be able to see TG Footer', () => {
-        cy.get('.has-text-centered > :nth-child(1) > p').should('be.visible')
-        cy.get('.has-text-centered > :nth-child(1) > p').should('have.text', '2860 S River Rd Suite 480, Des Plaines, IL 60018')
+        homePage.addressValidation('2860 S River Rd Suite 480, Des Plaines, IL 60018')
 
-        cy.get('#Footer_facebook__UiT2a > path').should('be.visible')
-        cy.get('[href="https://www.facebook.com/techglobaleducation"]').should('not.be.empty')
+        homePage.facebookLinkValidation()
 
-        cy.get('#Footer_instagram__0NXVy').should('be.visible')
-        cy.get('[href="https://www.instagram.com/techglobal.school/?hl=en"]').should('not.be.empty')
+        homePage.instagramLinkValidation()
 
-        cy.get('.has-text-centered > :nth-child(3) > p').should('be.visible')
-        cy.get('.has-text-centered > :nth-child(3) > p').should('have.text', '© 2023 TechGlobal - All Rights Reserved')
+        homePage.copyrightValidation('© 2023 TechGlobal - All Rights Reserved')
     })
 })
---
+++ cypress/page_objects/TGHomePage.js
@@ -0,0 +1,30 @@
+export class TGHomePage {
+    navigate() {
+        cy.visit('https://techglobal-training.com/') // Adding '?delay-new-todo=5000' to the end of the URL will delay the 'new-todo' element from appearing for 5 seconds
+    }
+
+    logoVisibility() {
+        cy.get('.Header_headerContainer__kKEW0 > :nth-child(1) > #logo > img').should('be.visible')
+        // The {timeout: XXXX} parameter can be added as the second parameter in the 'get' method to increase the default timeout of 4 seconds
+    }
+
+    addressValidation(expectedText) {
+        cy.get('.has-text-centered > :nth-child(1) > p').should('be.visible')
+        cy.get('.has-text-centered > :nth-child(1) > p').should('have.text', expectedText)
+    }
+
+    facebookLinkValidation() {
+        cy.get('#Footer_facebook__UiT2a > path')
+        cy.get('[href="https://www.facebook.com/techglobaleducation"]')
+    }
+
+    instagramLinkValidation() {
+        cy.get('#Footer_instagram__0NXVy')
+        cy.get('[href="https://www.instagram.com/techglobal.school/?hl=en"]')
+    }
+
+    copyrightValidation(expectedText) {
+        cy.get('.has-text-centered > :nth-child(3) > p').should('be.visible')
+        cy.get('.has-text-centered > :nth-child(3) > p').should('have.text', expectedText)
+    }
+}
