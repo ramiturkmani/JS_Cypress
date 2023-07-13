00665076a49e9f2bb2c91c6bb2809b9802e2f95d
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-12 23:44:05 -0500

    integrated mocha test grouping

--- cypress/integration/TGMockInterview.spec.js
+++ cypress/integration/TGMockInterview.spec.js
@@ -1,12 +1,16 @@
 /// <reference types="cypress"/>
 
-it('should be able to fill out mock interview form', () => {
-    cy.visit('https://techglobal-training.com/') 
-
-    cy.get('.navbar-brand > .active').click()
-    cy.get('#mock-option').click()
-
-    cy.get(':nth-child(2) > input').type('ramiturkmani')
-    cy.get(':nth-child(4) > input').type('techglobal123')
-    cy.get('button').click()
-}) 
+describe('mock interview functionality', () => {
+    it('should be able to navigate to the mock interview page', () => {
+        cy.visit('https://techglobal-training.com/')
+    
+        cy.get('.navbar-brand > .active').click()
+        cy.get('#mock-option').click()
+    })
+    
+    it('should be able to fill out mock interview form', () => {
+        cy.get(':nth-child(2) > input').type('ramiturkmani')
+        cy.get(':nth-child(4) > input').type('techglobal123')
+        cy.get('button').click()
+    }) 
+})
