4bfe231c87f0309be739781c170723c9cf64890e
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-14 23:21:36 -0500

    slight revision

--- cypress/integration/TGMockInterview.spec.js
+++ cypress/integration/TGMockInterview.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress"/>
 
 it('should be able to navigate to the mock interview page', () => {
-        cy.visit('https://techglobal-training.com/')
+    cy.visit('https://techglobal-training.com/')
 
-        cy.get('.navbar-brand > .active').click()
-        cy.get('#mock-option').click()
-        
-        cy.get(':nth-child(2) > input').type('ramiturkmani')
-        cy.get(':nth-child(4) > input').type('techglobal123')
-        cy.get('button').click()
-    })
+    cy.get('.navbar-brand > .active').click()
+    cy.get('#mock-option').click()
+
+    cy.get(':nth-child(2) > input').type('ramiturkmani')
+    cy.get(':nth-child(4) > input').type('techglobal123')
+    cy.get('button').click()
+})
