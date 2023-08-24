c6a19bfe76c69fa3731d2235e49c63318ff2e7f1
ramiturkmani <ramiturkmani1@gmail.com>
2023-08-23 23:51:35 -0500

    slight revision

--- cypress/integration/TGMockInterview.spec.js
+++ cypress/integration/TGMockInterview.spec.js
@@ -5,7 +5,9 @@ it('should be able to navigate to the mock interview page', () => {
 
     cy.get('.navbar-brand > .active').click()
     cy.get('#mock-option').click()
+
     cy.get(':nth-child(2) > input').type('ramiturkmani')
     cy.get(':nth-child(4) > input').type('techglobal123')
+    
     cy.get('button').click()
 })
